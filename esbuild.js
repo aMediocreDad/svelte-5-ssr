70348cbb1cb2d5b2de40781cb0693a1fbabf9f7e
aMediocreDad <amediocredad@outlook.com>
2023-11-11 00:10:49 +0100

    fix: CSS

--- esbuild.js
+++ esbuild.js
@@ -1,49 +1,45 @@
 import { build as esBuild } from "esbuild";
 import sveltePlugin from "esbuild-svelte";
 
-const config = {
-	entryPoints: ["src/app.svelte"],
-	platform: "node",
-	format: "cjs",
-	bundle: true,
-	write: false,
-	plugins: [
-		sveltePlugin({
-			compilerOptions: {
-				runes: true,
-				generate: "server",
-				css: "injected",
-			},
-		}),
-	],
-};
-
-const clientConfig = {
-	platform: "browser",
-	format: "esm",
-	outdir: "public",
-	bundle: true,
-	write: true,
-	entryNames: "app",
-	plugins: [
-		sveltePlugin({
-			compilerOptions: {
-				runes: true,
-				generate: "client",
-				css: "external",
-			},
-		}),
-	],
-};
-
-export const buildSsr = async () => esBuild(config);
-export const buildClient = async (string) =>
+export const buildSsr = async () =>
 	esBuild({
-		...clientConfig,
+		entryPoints: ["src/app.svelte"],
+		platform: "node",
+		format: "cjs",
+		bundle: true,
+		write: false,
+		plugins: [
+			sveltePlugin({
+				compilerOptions: {
+					runes: true,
+					generate: "server",
+					css: "injected",
+				},
+			}),
+		],
+	});
+
+export const buildClient = async (contents) =>
+	esBuild({
 		stdin: {
-			contents: string,
+			contents,
 			sourcefile: "app.js",
 			resolveDir: "./src",
 			loader: "js",
 		},
+		entryNames: "app",
+		platform: "browser",
+		format: "esm",
+		outdir: "public",
+		bundle: true,
+		write: true,
+		plugins: [
+			sveltePlugin({
+				compilerOptions: {
+					runes: true,
+					generate: "client",
+					css: "external",
+				},
+			}),
+		],
 	});
